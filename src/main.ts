698699bf9fb4f2e3246b7cb8b53974f809972cdd
Neil Kuan <guan840912@gmail.com>
2021-10-01 15:32:28 +0800

    feat(lambda-arm): support lambda arm Architecture, check function

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,35 @@
-import { AWSManagedPolicies } from './';
-export class Hello {
-  constructor(public name: string) {
-    console.log(`Hello ${name} ${AWSManagedPolicies.AMAZON_SSM_MANAGED_INSTANCE_CORE}`);
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as cdk from '@aws-cdk/core';
+
+export interface LambdaArmFunctionProps extends lambda.FunctionProps {
+
+}
+
+export class LambdaArmFunction extends cdk.Construct {
+  public readonly lambdaFunction: lambda.Function;
+  constructor(scope: cdk.Construct, id: string, props:LambdaArmFunctionProps) {
+    super(scope, id);
+
+    if (props.runtime === lambda.Runtime.NODEJS_12_X) {
+      cdk.Annotations.of(this).addWarning('You are using Node.js 12.x at ARM');
+    } else if (props.runtime === lambda.Runtime.NODEJS_14_X) {
+      cdk.Annotations.of(this).addWarning('You are using Node.js 14.x at ARM');
+    } else if (props.runtime === lambda.Runtime.PYTHON_3_8) {
+      cdk.Annotations.of(this).addWarning('You are using Python 3.8 at ARM');
+    } else if (props.runtime === lambda.Runtime.PYTHON_3_9) {
+      cdk.Annotations.of(this).addWarning('You are using Python 3.9 at ARM');
+    } else if (props.runtime === lambda.Runtime.JAVA_8_CORRETTO) {
+      cdk.Annotations.of(this).addWarning('You are using Java 8 on al2 at ARM');
+    } else if (props.runtime === lambda.Runtime.JAVA_11) {
+      cdk.Annotations.of(this).addWarning('You are using Java 11 at ARM');
+    } else if (props.runtime === lambda.Runtime.DOTNET_CORE_3_1) {
+      cdk.Annotations.of(this).addWarning('You are using DOTNET CORE 3.1 at ARM');
+    } else if (props.runtime === lambda.Runtime.RUBY_2_7) {
+      cdk.Annotations.of(this).addWarning('You are using RUBY 2.7 at ARM');
+    } else {
+      throw new Error(`Invalid Runtime ${props.runtime} at ARM, See https://docs.aws.amazon.com/lambda/latest/dg/foundation-arch.html?icmpid=docs_lambda_rss`);
+    }
+
+    this.lambdaFunction = new lambda.Function(this, 'LambdaFunction', { architectures: [lambda.Architecture.ARM_64], ...props });
   }
 }
--- test/common.test.ts
+++ test/common.test.ts
@@ -1,7 +1,9 @@
 import * as iam from '@aws-cdk/aws-iam';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as s3 from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 import '@aws-cdk/assert/jest';
-import { AWSManagedPolicies } from '../src/managed-policies';
+import { AWSManagedPolicies, LambdaArmFunction } from '../src/index';
 
 test('Has Role Attach Managed Role', () => {
   const mockApp = new cdk.App();
@@ -10,5 +12,39 @@ test('Has Role Attach Managed Role', () => {
     assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
   });
   role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName(AWSManagedPolicies.AMAZON_SSM_MANAGED_INSTANCE_CORE));
-  expect(stack).not.toHaveResourceLike('AWS::S3::Bucket');
+
+  const runtimeList = [lambda.Runtime.NODEJS_12_X, lambda.Runtime.NODEJS_14_X, lambda.Runtime.PYTHON_3_8,
+    lambda.Runtime.PYTHON_3_9, lambda.Runtime.JAVA_8_CORRETTO, lambda.Runtime.JAVA_11, lambda.Runtime.DOTNET_CORE_3_1,
+    lambda.Runtime.RUBY_2_7];
+  const bk = new s3.Bucket(stack, 'thebucket');
+  runtimeList.forEach((runtime, index) => {
+    new LambdaArmFunction(stack, `aaa${index}`, {
+      runtime: runtime,
+      handler: 'index.handler',
+      code: lambda.Code.fromBucket(bk, 'test' ),
+    });
+  });
+
+  expect(stack).toHaveResource('AWS::S3::Bucket');
+  expect(stack).toHaveResource('AWS::Lambda::Function', {
+    Architectures: [
+      'arm64',
+    ],
+  });
+});
+
+test('fail use go runtime on arm', () => {
+  const mockApp = new cdk.App();
+  const stack = new cdk.Stack(mockApp, 'testing-stack');
+  expect(()=>{
+    new LambdaArmFunction(stack, 'aaa', {
+      runtime: lambda.Runtime.GO_1_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+import platform
+def handler(event, contexts):
+    arch = platform.system()
+    return { "arch": str(arch)}`),
+    });
+  }).toThrowError('Invalid Runtime go1.x at ARM, See https://docs.aws.amazon.com/lambda/latest/dg/foundation-arch.html?icmpid=docs_lambda_rss');
 });
