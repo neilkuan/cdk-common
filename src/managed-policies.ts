dafa4ce396136bfb014deedcf104b1201b947c9a
Neil Kuan <46012524+neilkuan@users.noreply.github.com>
2024-10-04 08:15:16 +0800

    chore(deps): upgrade dependencies (#2006)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/neilkuan/cdk-common/actions/runs/11171628990
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*

--- src/managed-policies.ts
+++ src/managed-policies.ts
@@ -1230,4 +1230,5 @@ export enum AWSManagedPolicies {
   'AWSDIRECTORY_SERVICE_DATA_FULL_ACCESS' = 'AWSDirectoryServiceDataFullAccess',
   'AWSDIRECTORY_SERVICE_DATA_READ_ONLY_ACCESS' = 'AWSDirectoryServiceDataReadOnlyAccess',
   'QAPPS_SERVICE_ROLE_POLICY' = 'aws-service-role/QAppsServiceRolePolicy',
+  'AMAZON_EKS_WORKER_NODE_MINIMAL_POLICY' = 'AmazonEKSWorkerNodeMinimalPolicy',
 };
