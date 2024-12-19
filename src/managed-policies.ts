08154b74ac2ee6c286051ccf8a6d4527a24ff25c
Neil Kuan <46012524+neilkuan@users.noreply.github.com>
2024-12-19 08:15:57 +0800

    chore(deps): upgrade dependencies (#2156)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/neilkuan/cdk-common/actions/runs/12403550825
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*

--- src/managed-policies.ts
+++ src/managed-policies.ts
@@ -1315,4 +1315,6 @@ export enum AWSManagedPolicies {
   'AMAZON_SAGE_MAKER_TRAINING_PLAN_CREATE_ACCESS' = 'AmazonSageMakerTrainingPlanCreateAccess',
   'AMAZON_SAGE_MAKER_CANVAS_SM_DATA_SCIENCE_ASSISTANT_ACCESS' = 'AmazonSageMakerCanvasSMDataScienceAssistantAccess',
   'AWSPARTNER_CENTRAL_SELLING_RESOURCE_SNAPSHOT_JOB_EXECUTION_ROLE_POLICY' = 'AWSPartnerCentralSellingResourceSnapshotJobExecutionRolePolicy',
+  'AWSBACKUP_SERVICE_ROLE_POLICY_FOR_INDEXING' = 'AWSBackupServiceRolePolicyForIndexing',
+  'AWSBACKUP_SERVICE_ROLE_POLICY_FOR_ITEM_RESTORES' = 'AWSBackupServiceRolePolicyForItemRestores',
 };
